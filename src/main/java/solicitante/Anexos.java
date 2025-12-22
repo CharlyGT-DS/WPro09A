2e3e36355f01773a5a4b776ffdba24d2602cb0a9
GtJonatan <jonaed64@gmail.com>
2025-12-21 23:38:22 -0600

    actualizando proyecto

--- src/main/java/solicitante/Anexos.java
+++ src/main/java/solicitante/Anexos.java
@@ -1,30 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solicitante;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.PrimeFaces;
+import org.primefaces.event.FileUploadEvent;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.faces.event.ActionEvent;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
-import org.primefaces.PrimeFaces;
-import org.primefaces.event.FileUploadEvent;
-
-/**
- *
- * @author WINDOWS
- */
 
 @ViewScoped
 @Named("anexos")
 public class Anexos implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private List<DocumentoSolicitante> documentos;
     private DocumentoSolicitante documentoSeleccionado;
+    private DocumentoSolicitante documentoParaPrevisualizar;
 
     @PostConstruct
     public void init() {
@@ -33,38 +31,88 @@ public class Anexos implements Serializable {
         documentos.add(new DocumentoSolicitante("Documento 2"));
     }
 
-    public void seleccionarDocumento(ActionEvent event) {
-        documentoSeleccionado = (DocumentoSolicitante)
-                event.getComponent().getAttributes().get("doc");
-    }
-
     public void subirArchivo(FileUploadEvent event) {
+
         if (documentoSeleccionado == null) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Advertencia", "No se ha seleccionado un documento"));
             return;
         }
 
-        documentoSeleccionado.setArchivo(event.getFile().getContent());
-        documentoSeleccionado.setNombreArchivo(event.getFile().getFileName());
+        try {
+            documentoSeleccionado.setArchivo(event.getFile().getContent());
+            documentoSeleccionado.setNombreArchivo(event.getFile().getFileName());
+            documentoSeleccionado.setTipoArchivo(event.getFile().getContentType());
+
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO,
+                            "Éxito", "Archivo subido correctamente"));
+
+            PrimeFaces.current().executeScript("PF('dlgUpload').hide()");
+
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "Error", "No se pudo subir el archivo"));
+            e.printStackTrace();
+        }
     }
 
-    public void verArchivo(DocumentoSolicitante doc) {
-        // lógica para ver o descargar
+    public void previsualizarArchivo(ActionEvent event) {
+        documentoParaPrevisualizar = (DocumentoSolicitante) event.getComponent().getAttributes().get("doc");
+
+        FacesContext.getCurrentInstance()
+                .getExternalContext()
+                .getSessionMap()
+                .put("anexos", this);
+
+        PrimeFaces.current().executeScript("PF('dlgPreview').show()");
     }
-    
+
+    /* PREPARAR DIALOG DE SUBIDA */
     public void prepararDialog(ActionEvent event) {
-    documentoSeleccionado = (DocumentoSolicitante)
-            event.getComponent().getAttributes().get("doc");
 
-    PrimeFaces.current().ajax().update("frmAnexos:dlgUpload");
-    PrimeFaces.current().executeScript("PF('dlgUpload').show()");
-}
+        documentoSeleccionado
+                = (DocumentoSolicitante) event.getComponent()
+                        .getAttributes()
+                        .get("doc");
 
-    // getters / setters
+        PrimeFaces.current().ajax().update("frmAnexos:dlgUpload");
+        PrimeFaces.current().executeScript("PF('dlgUpload').show()");
+    }
+
+    public boolean tieneArchivo(DocumentoSolicitante doc) {
+        return doc != null
+                && doc.getArchivo() != null
+                && doc.getArchivo().length > 0;
+    }
+
+    public boolean esImagen() {
+        return documentoParaPrevisualizar != null
+                && documentoParaPrevisualizar.getTipoArchivo() != null
+                && documentoParaPrevisualizar.getTipoArchivo().startsWith("image/");
+    }
+
+    public boolean esPDF() {
+        return documentoParaPrevisualizar != null
+                && documentoParaPrevisualizar.getTipoArchivo() != null
+                && "application/pdf".equals(documentoParaPrevisualizar.getTipoArchivo());
+    }
+
     public List<DocumentoSolicitante> getDocumentos() {
         return documentos;
     }
 
     public DocumentoSolicitante getDocumentoSeleccionado() {
         return documentoSeleccionado;
+    }
+
+    public DocumentoSolicitante getDocumentoParaPrevisualizar() {
+        return documentoParaPrevisualizar;
+    }
+
+    public void setDocumentoParaPrevisualizar(DocumentoSolicitante documentoParaPrevisualizar) {
+        this.documentoParaPrevisualizar = documentoParaPrevisualizar;
     }
 }
--- src/main/java/solicitante/DocumentoSolicitante.java
+++ src/main/java/solicitante/DocumentoSolicitante.java
@@ -1,57 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solicitante;
 
-/**
- *
- * @author WINDOWS
- */
-public class DocumentoSolicitante {
 
-    private int ordenLista;
+public class DocumentoSolicitante {
+    
     private String nombre;
     private byte[] archivo;
     private String nombreArchivo;
-
+    private String tipoArchivo;
+    
+    public DocumentoSolicitante() {
+    }
+    
     public DocumentoSolicitante(String nombre) {
         this.nombre = nombre;
     }
-
-    public boolean isTieneArchivo() {
+    
+      public boolean isTieneArchivo() {
         return archivo != null;
     }
 
-    public int getOrdenLista() {
-        return ordenLista;
-    }
-
-    public void setOrdenLista(int ordenLista) {
-        this.ordenLista = ordenLista;
-    }
-    
     public String getNombre() {
         return nombre;
     }
-
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
     public byte[] getArchivo() {
         return archivo;
     }
-
+    
     public void setArchivo(byte[] archivo) {
         this.archivo = archivo;
     }
-
+    
     public String getNombreArchivo() {
         return nombreArchivo;
     }
-
+    
     public void setNombreArchivo(String nombreArchivo) {
         this.nombreArchivo = nombreArchivo;
+    }
+    
+    public String getTipoArchivo() {
+        return tipoArchivo;
+    }
+    
+    public void setTipoArchivo(String tipoArchivo) {
+        this.tipoArchivo = tipoArchivo;
     }
 }
